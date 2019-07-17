fd69fc965878b2d5914d0b290c03b23afaf56bf1
TY <yossanan.s@rama3honda.com>
2019-07-17 17:02:58 +0700

    forget rename variable

--- py_parameter.py
+++ py_parameter.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     print ("Please tell something about you")
     you = input('Who are you? : ')
     which_diff = input('Which Defferance you and me? : ')  
-    verbs = str(sys.argv[2])  # pass parameter from command
+    me = str(sys.argv[2])  # pass parameter from command
     
     allstr = "%s %s %s" % (you,which_diff,me)
     print(allstr)
